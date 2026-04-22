c0a67630e2e39a7e2ea321e12fdcf1d5ba684616
Neha-ruthla <lalitaruthla046@gmail.com>
2026-04-22 22:45:52 +0530

    Add files via upload

---
+++ to do app/script.js
@@ -0,0 +1,17 @@
+let inputBox = document.querySelector('#inputBox') 
+let addBtn = document.querySelector('#addBtn')
+let list = document.querySelector('#list')
+
+addBtn.addEventListener('click', function(){
+  let eachlist = document.createElement('div')
+  eachlist.setAttribute('class','eachList')            
+
+eachlist.innerHTML = <P>${inputBox.value}</P>
+  
+
+
+  eachlist.textContent
+ 
+console.log(eachList)
+
+})
